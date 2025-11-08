7e41574d6cc324eaa430f18a4412de293bf8b56f
shaked18 <shaked-adi@campus.technion.ac.il>
2025-11-08 10:49:19 +0200

    missing q1.4

--- HW1/hw1/answers.py
+++ HW1/hw1/answers.py
@@ -11,8 +11,14 @@ math (delimited with $$).
 part1_q1 = r"""
 **Your answer:**
 1. false. 
+Not every disjoint split is equally useful. The train and test sets should be balanced and representative of the true data distribution.
+For example, if a training set contains only cat images while the test set includes both cats and dogs the model wont learn the “dog” class, since we cannot learn what we dont see and will fail to generalize.
+
 2. false.
+The test set must remain untouched during cross validation. Letting the test set influences any decision will causes leakage, overfitting to the test and biased estimate of generalization.
+
 3. true.
+In cross validation, each fold’s validation performance is indeed a proxy for the model’s generalization beacuse it mimics training and evaluating new sampels.
 4. false.
 
 Write your answer using **markdown** and $\LaTeX$:
@@ -26,7 +32,7 @@ An equation: $e^{i\pi} -1 = 0$
 
 part1_q2 = r"""
 **Your answer:**
-he is dumb.
+His approach isnt justified. When he chooses the value of $\lambda$ based on the test set performance, he is letting the test set influence the model decisions. That turns the test set into the validation set, causing data leakage and biased estimate of generalization.
 
 Write your answer using **markdown** and $\LaTeX$:
 ```python
