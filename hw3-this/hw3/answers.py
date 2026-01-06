ad1603f7b2079e924d015c66fc15a127540731ee
UriKH <urikh@icloud.com>
2026-01-06 17:12:13 +0200

    update params

--- hw3-this/hw3/answers.py
+++ hw3-this/hw3/answers.py
@@ -11,25 +11,17 @@ math (delimited with $$).
 
 def part1_rnn_hyperparams():
     hypers = dict(
-        batch_size=0,
-        seq_len=0,
-        h_dim=0,
-        n_layers=0,
-        dropout=0,
-        learn_rate=0.0,
-        lr_sched_factor=0.0,
-        lr_sched_patience=0,
+        batch_size=32,
+        seq_len=64,
+        h_dim=1024,
+        n_layers=3,
+        dropout=0.2,
+        learn_rate=0.01,
+        lr_sched_factor=0.5,
+        lr_sched_patience=5,
     )
     # TODO: Set the hyperparameters to train the model.
     # ====== YOUR CODE: ======
-    batch_size=0,
-    seq_len=0,
-    h_dim=0,
-    n_layers=0,
-    dropout=0,
-    learn_rate=0.0,
-    lr_sched_factor=0.0,
-    lr_sched_patience=0,
     # ========================
     return hypers
 
