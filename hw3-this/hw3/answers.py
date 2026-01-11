74d3396f0326f9c17997445690c966c2ef365388
shaked18 <shaked-adi@cs.technion.ac.il>
2026-01-11 13:46:46 +0200

    part 1 q

--- hw3-this/hw3/answers.py
+++ hw3-this/hw3/answers.py
@@ -22,14 +22,14 @@ def part1_rnn_hyperparams():
     )
     # TODO: Set the hyperparameters to train the model.
     # ====== YOUR CODE: ======
-    hypers["batch_size"] = 32
-    hypers["seq_len"] = 64
-    hypers["h_dim"] = 512
+    hypers["batch_size"] = 64
+    hypers["seq_len"] = 128
+    hypers["h_dim"] = 128
     hypers["n_layers"] = 3
-    hypers["dropout"] = 0.2
-    hypers["learn_rate"] = 0.01
+    hypers["dropout"] = 0.4
+    hypers["learn_rate"] = 3e-4
     hypers["lr_sched_factor"] = 0.5
-    hypers["lr_sched_patience"] = 0.5
+    hypers["lr_sched_patience"] = 2
     # ========================
     return hypers
 
